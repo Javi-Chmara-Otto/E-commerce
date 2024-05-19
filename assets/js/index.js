1d02302f7470c4f42693899a8b32029305d3c7d6
Javier Chmara Otto <javierchmaraotto@gmail.com>
2024-05-19 20:55:37 -0300

    componetizacion de estilos

--- assets/js/index.js
+++ assets/js/index.js
@@ -37,11 +37,13 @@
 
 // FUNCIÓN CALLBACK
 
-const saludar = (nombre) => alert('Hola ' + nombre);
+// const saludar = (nombre) => alert('Hola ' + nombre);
 
-const procesarEntradaUsuario = callback => {
-    let nombre = prompt('Por favor ingresa tu nombre')
-    callback(nombre)
-}
+// const procesarEntradaUsuario = callback => {
+//     let nombre = prompt('Por favor ingresa tu nombre')
+//     callback(nombre)
+// }
 
-procesarEntradaUsuario(saludar);
+// procesarEntradaUsuario(saludar);
+
+
